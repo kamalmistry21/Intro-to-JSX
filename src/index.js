86f67a8773e655e91276dcdd82cb6d6f9c8b0139
Kamal Mistry <31496227+kamalmistry21@users.noreply.github.com>
2022-05-23 00:01:46 +1200

    React Setup Initial react setup

---
+++ src/index.js
@@ -0,0 +1,12 @@
+//Can use import instead of require like bellow
+import React from "react";
+import ReactDOM from "react-dom";
+
+//How to create and display an HTML element
+ReactDOM.render(
+  <div>
+    <h1>Hello World!</h1>
+    <p>This is a paragraph</p>
+  </div>,
+  document.getElementById("root")
+);
